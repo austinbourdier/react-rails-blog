e25a1e979468adbc2a1b2e1750708676927cdb52
Austin Bourdier <austinbourdier@compology.us>
2017-04-06 14:10:49 -0700

    pushing for debugging

--- config/application.rb
+++ config/application.rb
@@ -12,8 +12,9 @@ module RailsReactBoilerplate
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    config.assets.enabled = false
 
+    config.assets.precompile += %w(app.js)
+    config.assets.precompile += %w(app.vendor.js)
     config.generators do |generator|
       generator.assets false
     end
