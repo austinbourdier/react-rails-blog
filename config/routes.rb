d13e611a9c3dd9d7feb2986702b5681f854f7777
Austin Bourdier <austinbourdier@compology.us>
2017-04-06 13:39:48 -0700

    pushing for debugging

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   get 'post/posts'
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '*path', to: 'home#index'
+  get 'create', to: 'home#index'
 end
